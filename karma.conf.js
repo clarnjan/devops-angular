d62c1ad48a0ff983642962bf69ac89cdb6cb8575
damjan <damjan@Ubuntu22.myguest.virtualbox.org>
2024-06-16 18:32:52 +0200

    browserless

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Firefox'],
+      browsers: [],
       singleRun: false,
       restartOnFileChange: true
     });
